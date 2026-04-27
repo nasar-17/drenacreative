8c453435bd82454700df66c176ebc648c5ced29e
nasar-17 <nasaruddinpawe@gmail.com>
2026-04-28 05:47:57 +0800

    mengubah path logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -58,7 +58,7 @@ export function Navbar() {
           }`}
         >
           <img
-            src="src/assets/Logo.png"
+            src="/Logo.png"
             className="w-8 h-8 md:w-10 md:h-10 object-contain shrink-0"
             alt="Logo"
           />
